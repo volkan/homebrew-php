a4c5f7814ebc34104e9ccefcb301f72fe9006f5f
Volkan Altan <volkanaltan@gmail.com>
2012-11-18 15:30:32 +0200

    added igbinary support to memcached

--- Formula/php53-memcached.rb
+++ Formula/php53-memcached.rb
@@ -1,13 +1,40 @@
 require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 
 class Php53Memcached < AbstractPhp53Extension
-  init
   homepage 'http://pecl.php.net/package/memcached'
   url 'http://pecl.php.net/get/memcached-2.0.1.tgz'
   sha1 '5442250bf4a9754534bce9a3033dc5363d8238f7'
   head 'https://github.com/php-memcached-dev/php-memcached.git'
 
+  depends_on 'autoconf' => :build
   depends_on 'libmemcached'
+  depends_on 'php53' unless build.include?('without-homebrew-php')
+
+  if ARGV.include? '--with-igbinary'
+      depends_on 'php53-igbinary'
+      puts "if get any error, you should run 'pecl install igbinary'"
+  end
+  
+  def options
+   [
+     ['--with-igbinary', 'Include igbinary support'],     
+   ]
+  end
+
+  def configure_args
+    args = [
+      "--prefix=#{prefix}",        
+      phpconfig,
+      "--with-libmemcached-dir=#{Formula.factory('libmemcached').prefix}"    
+    ]
+
+    # Enable igbinary
+    if ARGV.include? '--with-igbinary'
+      args.push "--enable-memcached-igbinary"
+    end
+
+    return args
+  end
 
   def install
     Dir.chdir "memcached-#{version}" unless build.head?
@@ -16,9 +43,7 @@ class Php53Memcached < AbstractPhp53Extension
     ENV.universal_binary
 
     safe_phpize
-    system "./configure", "--prefix=#{prefix}",
-                          phpconfig,
-                          "--with-libmemcached-dir=#{Formula.factory('libmemcached').prefix}"
+    system "./configure", *configure_args 
     system "make"
     prefix.install "modules/memcached.so"
     write_config_file unless build.include? "without-config-file"
