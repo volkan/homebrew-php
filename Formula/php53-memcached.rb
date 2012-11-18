62b5d9da68fc16702f4bbada9966f1079fe8cca7
Volkan Altan <volkanaltan@gmail.com>
2012-11-19 00:25:14 +0200

    removed some line

--- Formula/php53-memcached.rb
+++ Formula/php53-memcached.rb
@@ -1,14 +1,13 @@
 require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 
 class Php53Memcached < AbstractPhp53Extension
+  init
   homepage 'http://pecl.php.net/package/memcached'
   url 'http://pecl.php.net/get/memcached-2.0.1.tgz'
   sha1 '5442250bf4a9754534bce9a3033dc5363d8238f7'
   head 'https://github.com/php-memcached-dev/php-memcached.git'
 
-  depends_on 'autoconf' => :build
-  depends_on 'libmemcached'
-  depends_on 'php53' unless build.include?('without-homebrew-php')
+  depends_on 'libmemcached'  
 
   if ARGV.include? '--with-igbinary'
       depends_on 'php53-igbinary'
